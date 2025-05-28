438a63395d2b0dae33e3cb8a3171ec6fc64f6651
Jacob Fields <jacobfields@ou.edu>
2025-05-28 11:08:08 -0500

    Update StitchBlobs.sh

---
+++ StitchBlobs.sh
@@ -0,0 +1,7 @@
+StitchBlobs --in_list "blocktag_file.txt" \
+            --out_list "blockid_file.txt" \
+            --var "block_tag" \
+            --latname "latitude" \
+            --lonname "longitude" \
+            --mintime "4d" \
+            --min_overlap_prev 20 \
